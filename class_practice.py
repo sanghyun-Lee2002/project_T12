59cb75bfa7c7c4777f40de6059f7f2e4eccf3c5e
kimwoolina <rladnfls0500@gmail.com>
2024-07-04 02:35:49 +0900

    hash password

--- class_practice.py
+++ class_practice.py
@@ -1,16 +1,26 @@
+import hashlib
+
 class Member:
     def __init__(self, name, username, password):
         self.name = name            # 회원 이름
         self.username = username    # 회원 아이디
-        self.password = password
+        self.password = self.encrypt_pw(password)
 
+    # 비밀번호 암호화
+    def encrypt_pw(self, password):
+        hash_obj = hashlib.sha256()         # SHA-256 해시 객체 생성
+        hash_obj.update(password.encode())  # 데이터 업데이트
+        hash_value = hash_obj.hexdigest()   # 해시 값 추출
+        return hash_value
+    
     # 회원 정보 print
     def display(self):
-        print(f"회원 이름 : {self.name}, 회원 아이디: {self.username}")
+        pass
 
 
 class Post:
     pass
+
 
 # Members 리스트 생성 및 회원 인스턴스 추가
 members = []
@@ -24,7 +34,6 @@ posts = []
 
 
 # 특정 유저가 작성한 게시글의 제목 프린트
-username_to_check = "kimwoolina"
 
 
 # 특정 단어가 content에 포함된 게시글의 제목 프린트
